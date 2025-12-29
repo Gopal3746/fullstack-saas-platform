16c7cdcac0f4eb181f7e802f24bf0a85a60b7de3
Gopal3746 <gopalkamineni2001@gmail.com>
2025-12-29 14:51:52 -0500

    Fix CI: force H2 datasource for Spring context test

--- backend/src/test/java/com/app/jiralite/JiraliteApplicationTests.java
+++ backend/src/test/java/com/app/jiralite/JiraliteApplicationTests.java
@@ -4,7 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=PostgreSQL",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.open-in-view=false"
+})
 @ActiveProfiles("test")
 class JiraliteApplicationTests {
 
